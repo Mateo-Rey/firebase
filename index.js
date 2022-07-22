7bb451bab6274d84c10d9541e54685abd18c422f
Mateo Rey <mateorey@Mateos-MacBook-Air.local>
2022-07-22 15:23:21 -0400

    read from firestore 3 ways

--- index.js
+++ index.js
@@ -8,5 +8,44 @@ initializeApp({
 
 const db = getFirestore()
 
-const car = { make: 'Audi', model: "A3", year: 2018, color: 'grey'}
-db.collection('cars').add(car)
+// const car = { make: 'Ferrari', model: "GTO", year: 2008, color: 'red'}
+
+// db.collection('cars').add(car)
+//     .then(doc => {
+//         console.log('Doc added', doc.id)
+//     })
+//     .catch(err => console.error(err))
+
+//     db.collection('cars').doc('lambo')
+//     .set ({make: 'lamborghini', model: "diablo", year: 2020, color: "red" })
+    
+// db.collection('cars').doc('lambo')
+//     .update({model: 'Diablo', color: 'hot pink'})
+
+
+// db.collection('cars').doc('lambo').get()
+//     .then(doc => {
+//         console.log(doc.data())
+//         console.log(doc.id)
+//     })
+//     .catch(console.log.error)
+
+// db.collection('cars').get()
+//     .then(collection => {
+//         collection.docs.map(doc => console.log(doc.data()))
+//         }) 
+//     .catch(console.error)
+
+db.collection('cars')
+    .where('year', '>=', 2015)
+    .get()
+        .then(collection => {
+            const cars = collection.docs.map(doc => doc.data())
+       console.log(cars)
+    })
+    .catch(console.error)
+// db.collection('cars')
+//     .where('year', '>', '2015')
+//     .get()
+//     .then(console.log('lucki'))
+//     .catch(console.error)
